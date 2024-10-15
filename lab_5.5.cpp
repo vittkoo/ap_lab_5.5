670c90e2afe5c6d2abec29453cd5c427399d1918
vittkoo <dmytrovych.vv@gmail.com>
2024-10-16 01:10:46 +0300

    commit_2

--- lab_5.5.cpp
+++ lab_5.5.cpp
@@ -2,9 +2,9 @@
 #include <iomanip>
 using namespace std;
 
-double toTwo(int number, int& depth);
-double toEight(int number, int& depth);
-double convertToBase(int number, int base, int& depth);
+double toTwo(int number, int level, int& depth);
+double toEight(int number, int level, int& depth);
+double convertToBase(int number, int base, int level, int& depth);
 
 int main()
 {
@@ -23,62 +23,70 @@ int main()
     }
 
     cout << fixed;
-    cout << "----------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------" << endl;
     cout << "|" << setw(10) << "System" << "  |"
-        << setw(10) << "Result" << "  |"
+        << setw(12) << "Result" << "  |"
         << setw(10) << "Depth" << "  |" << endl;
-    cout << "----------------------------------------------------" << endl;
-
+    cout << "--------------------------------------------------------" << endl;
 
     // Переведення у двійкову систему
     depth = 0;
-    double two = toTwo(number, depth);
+    double two = toTwo(number, 1, depth);
     cout << "|" << setw(10) << "Two" << "  |"
-        << setw(10) << setprecision(0) << two << "  |"
+        << setw(12) << setprecision(0) << two << "  |"
         << setw(10) << depth << "  |" << endl;
 
     // Переведення у вісімкову систему
     depth = 0;
-    double eight = toEight(number, depth);
+    double eight = toEight(number, 1, depth);
     cout << "|" << setw(10) << "Eight" << "  |"
-        << setw(10) << setprecision(0) << eight << "  |"
+        << setw(12) << setprecision(0) << eight << "  |"
         << setw(10) << depth << "  |" << endl;
 
+    // Переведення у систему з базою base
     depth = 0;
-    double convertedNumber = convertToBase(number, base, depth);
-
-    cout << "|" << setw(8) << "Base " << base << "  |"
-        << setw(10) << setprecision(0) << convertedNumber << "  |"
+    double convertedNumber = convertToBase(number, base, 1, depth);
+    cout << "|" << setw(9) << "Base " << base << "  |"
+        << setw(12) << setprecision(0) << convertedNumber << "  |"
         << setw(10) << depth << "  |" << endl;
 
-    cout << "----------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------" << endl;
 
     return 0;
 }
 
 // Рекурсивна функція для переведення у двійкову систему
-double toTwo(int number, int& depth)
+double toTwo(int number, int level, int& depth)
 {
-    if (number == 0) return 0;
+    if (level > depth) {
+        depth = level;
+    }
+    cout << "Level = " << level << endl;
 
-    depth++;
-    return toTwo(number / 2, depth) * 10 + (number % 2);
+    if (number == 0) return 0;
+    return toTwo(number / 2, level + 1, depth) * 10 + (number % 2);
 }
 
 // Рекурсивна функція для переведення у вісімкову систему
-double toEight(int number, int& depth)
+double toEight(int number, int level, int& depth)
 {
-    if (number == 0) return 0;
+    if (level > depth) {
+        depth = level;
+    }
+    cout << "Level = " << level << endl;
 
-    depth++;
-    return toEight(number / 8, depth) * 10 + (number % 8);
+    if (number == 0) return 0;
+    return toEight(number / 8, level + 1, depth) * 10 + (number % 8);
 }
 
 // Рекурсивна функція для переведення числа у систему з основою N
-double convertToBase(int number, int base, int& depth)
+double convertToBase(int number, int base, int level, int& depth)
 {
-    if (number == 0) return 0;
+    if (level > depth) {
+        depth = level;
+    }
+    cout << "Level = " << level << endl;
 
-    depth++;
-    return convertToBase(number / base, base, depth) * 10 + (number % base);
+    if (number == 0) return 0;
+    return convertToBase(number / base, base, level + 1, depth) * 10 + (number % base);
 }
